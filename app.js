8b9196254fdfaae47bc76f4761bc5b46ab013886
Mojo-Wojo <89461997+Mojo-Wojo@users.noreply.github.com>
2025-06-11 11:54:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(session({
   name: 'connect.sid',
   cookie: { path: '/' }
 }))
-app.use(csrf({ cookie: true }))
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(fileUpload());
